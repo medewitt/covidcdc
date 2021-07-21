a8aa9da2eb9f734e191d17f367bee19405a1fefe
Michael DeWitt <me.dewitt.jr@gmail.com>
2021-07-21 08:05:42 -0400

    initial commit

---
+++ scrape.R
@@ -0,0 +1,16 @@
+#!/usr/bin/Rscript
+setwd("C:/Users/54011/Documents/pandemic/cdc-scraper")
+download.file(
+  "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccinati
+on_county_condensed_data",
+sprintf("data/county-covid/%s.json", slugify::slugify(Sys.time())),
+quiet = TRUE,
+cacheOK = FALSE
+)
+
+download.file(
+"https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=integrated_county_latest_external_data",
+sprintf("data/county-detail/%s.json", slugify::slugify(Sys.time())),
+quiet = TRUE,
+cacheOK = FALSE
+)
