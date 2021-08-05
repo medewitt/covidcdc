c24cca4aa4e307cc384a1836ca4157564666007c
Michael DeWitt <me.dewitt.jr@gmail.com>
2021-08-05 19:28:38 -0400

    Update scrape.R
    fixing this part

--- scrape.R
+++ scrape.R
@@ -1,15 +1,23 @@
 #!/usr/bin/Rscript
+
+slugify_date <- function(x){
+	x <- stringi::stri_replace_all_regex(x,"[^\\P{P}-]","")
+	x <- gsub(x, pattern = " ", replacement = "-")
+	x
+}
+ping_time <- slugify_date(Sys.time())
+
 download.file(
   "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccinati
 on_county_condensed_data",
-sprintf("data/county-covid/%s.json", slugify::slugify(Sys.time())),
+sprintf("data/county-covid/%s.json", ping_time),
 quiet = TRUE,
 cacheOK = FALSE
 )
 
 download.file(
 "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=integrated_county_latest_external_data",
-sprintf("data/county-detail/%s.json", slugify::slugify(Sys.time())),
+sprintf("data/county-detail/%s.json", ping_time),
 quiet = TRUE,
 cacheOK = FALSE
 )
