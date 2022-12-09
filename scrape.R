a0e259c60f77753d3c95ef4820f4d2ac958a7b3d
Michael DeWitt <me.dewitt.jr@gmail.com>
2022-12-09 16:16:31 -0500

    Update scrape.R

--- scrape.R
+++ scrape.R
@@ -8,8 +8,7 @@ slugify_date <- function(x){
 ping_time <- slugify_date(Sys.time())
 
 try(download.file(
-  "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccinati
-on_county_condensed_data",
+  "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=integrated_county_latest_external_data",
 sprintf("data/county-covid/%s.json", ping_time),
 quiet = TRUE,
 cacheOK = FALSE
